2a25192c1e5241e47756eedb97fa04665cc8cad4
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2016-01-31 16:27:02 +0300

    Упдэйт

--- readmebuilder.py
+++ readmebuilder.py
@@ -7,18 +7,19 @@ FILENAME = "README.md"
 def build(filename):
     with open(filename, encoding="utf-8") as fp:
         content = fp.read()
-    matches = re.findall(r"/\*\*\s*([\s\S]+?)\s*\*/", content)
-    out = [
-        'Generated from source file: {}'.format(os.path.basename(filename)),
-        ''
-    ]
+    out = []
+    matches = re.findall(r"/\*\*\s+([\s\S]+?)\s+\*/\s+(.*function[^{]+)",
+                         content)
     re_asterisk = re.compile(r'^[ \t]*\* ?', re.M)
     re_at = re.compile(r'^@', re.M)
     for match in matches:
-        match = re_asterisk.sub('', match)
-        match = re_at.sub(' * ', match)
-        out.append(match)
-        out.append('')
+        description = re_asterisk.sub('', match[0])
+        description = re_at.sub('<br>&#64;', description)
+        function = match[1].strip()
+        function += " {}"
+        out.append(description)
+        out.append("")
+        out.append("```javascript\n" + function + "\n```\n")
     content = "\n".join(out)
     content = re.sub(r"<code>\s*([\s\S]+?)\s*</code>",
                      "```javascript\n\\1\n```",
