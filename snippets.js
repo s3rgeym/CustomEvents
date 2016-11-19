415020aa155cf09a7631c5f45435562f97aa5ed8
Senior YAML Developer <yamldeveloper@proton.me>
2016-11-19 20:10:40 +0300

    Update snippets.js

--- snippets.js
+++ snippets.js
@@ -1,25 +1,29 @@
-((params, indent) => {
+((params, retType = null, indent = 2) => {
+  const DEFAULT_TYPE = 'mixed';
+  retType = retType || DEFAULT_TYPE;
   params = params.split(/\s*,\s*/);
-  indent = " ".repeat(indent);
-  var out = [];
-  out.push(indent + "/**");
-  out.push(indent + " * Description goes here");
-  out.push(indent + " *");
-  for (var i = 0; i < params.length; ++i) {
-    out.push(indent + " * @param " + params[i] + " {type}");
+  indent = ' '.repeat(indent);
+  let out = [];
+  out.push(indent + '/**');
+  out.push(indent + ' * Description goes here.');
+  for (const param of params) {
+    const temp = param.split(/\s*:\s*/);
+    const name = temp[0];
+    const type = temp[1] || DEFAULT_TYPE;
+    out.push(`${indent} * @param {${type}} ${name} - The ${name} value.`);
   }
-  out.push(indent + " * @return {type}");
-  out.push(indent + " */")
-  prompt("Ctrl+C:", out.join("\n"));
-})("event, fn, scope, once", 2);
+  out.push(`${indent} * @return {${retType}} The ? value.`);
+  out.push(indent + ' */');
+  out = out.join('\n');
+  console.log(out);
+  prompt("Ctrl+C:", out);
+})('a: string, b: number, c');
 
 // Output:
   /**
-   * Description goes here
-   *
-   * @param event {type}
-   * @param fn {type}
-   * @param scope {type}
-   * @param once {type}
-   * @return {type}
+   * Description goes here.
+   * @param {string} a - The a value.
+   * @param {number} b - The b value.
+   * @param {mixed} c - The c value.
+   * @return {mixed} The ? value.
    */
