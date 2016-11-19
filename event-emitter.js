84a09685363f14d746f57c1126efbc73c2eef32f
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2016-11-19 20:22:25 +0300

    Hello, EcmaScript 6!

--- event-emitter.js
+++ event-emitter.js
@@ -2,139 +2,84 @@
 // @author Sergei Snegirev (yamldeveloper@proton.me)
 //
 
-/**
- * EventEmitter constructor
- */
-function EventEmitter() {
-  this._listeners = {};
-}
+// Пользовательские события
+// Частный случай реализации шаблона
+// https://en.wikipedia.org/wiki/Publish%E2%80%93subscribe_pattern
+// Интересная реализация:
+// https://fossies.org/linux/www/webkitgtk-2.15.1.tar.gz/webkitgtk-2.15.1/Source/WebInspectorUI/UserInterface/Base/Object.js#l_141
+class EventEmitter {
+  constructor() {
+    this._listeners = {};
+  }
 
-EventEmitter.prototype = {
+  // !Помните, что объявление стрелочной функции вида (x) => {} аналогично
+  // function (x) {}.bind(this), т.е. стрелочные функции захватывают контекст.
   /**
-   * Add event listener
    *
-   * @param event {string}
-   * @param fn {function}
-   * @param scope {object} (optional)
-   * @param once {boolean} (optional)
-   * @return {this}
+   * @param {string} type
+   * @param {function} listener
+   * @return {EventEmmiter}
    */
-  on: function(event, fn, scope, once) {
-    (this._hasListeners(event) ? this._listeners[event] :
-      this._listeners[event] = []).push({
-      fn: fn,
-      scope: scope != null ? scope : this,
-      once: once
-    });
+  on(type, listener) {
+    if ('function' !== typeof listener) {
+      throw new TypeError('listener must be a function');
+    }
+    if (!this._listeners.hasOwnProperty(type)) {
+      this._listeners[type] = [];
+    }
+    this._listeners[type].push(listener);
     return this;
-  },
+  }
+
   /**
-   * Add one-shot event listener
-   *
-   * @param event {string}
-   * @param fn {function}
-   * @param scope {object} (optional)
-   * @return {this}
-   */
-  once: function(event, fn, scope) {
-    return this.on(event, fn, scope, true);
-  },
-  /**
-   * Remove event listeners
-   *
-   *
-   * @param event {string} (optional)
-   * @param fn {function} (optional)
-   * @param scope {object} (optional)
-   * @return {this}
-   */
-  off: function(event, fn, scope) {
+    * off() --> remove all listeners
+    * off(type) --> remove all listeners of type
+    * off(type, listener) --> remove listener of type
+    * @param {string} type
+    * @param {function} listener
+    * @return {EventEmmiter}
+    */
+  off(type, listener) {
+    // удаляем всех слушателей
     if (!arguments.length) {
-      // .off() remove all listeners
       this._listeners = {};
-    } else if (arguments.length == 1) {
-      // .off(event) remove all listeners for event
-      if (this._hasListeners(event)) {
-        delete this._listeners[event];
+    } else if (1 === arguments.length) { // удалям слушателей с типом `type`
+      if (this._listeners.hasOwnProperty(type)) {
+        delete this._listeners[type];
       }
-    } else {
-      // .off(event, fn[, scope]) remove all listeners fn for event or
-      // remove all listeners fn for event in the scope
-      if (this._hasListeners(event)) {
-        var listeners = this._listeners[event];
-        var i = 0;
-        while (i < listeners.length) {
-          var listener = listeners[i];
-          if (fn === listener.fn &&
-              (scope == null || scope === listener.scope)) {
-            listeners.splice(i, 1);
-          } else {
-            ++i
-          }
+    } else if (this._listeners.hasOwnProperty(type)) {
+      // удаляем слушателя `listener` с типом `type`
+      const listeners = this._listeners[type];
+      let i = listeners.length;
+      while (i--) {
+        if (listener === listeners[i]) {
+          listeners.splice(i, 1);
         }
-        if (!listeners.length) {
-          delete this._listeners[event];
-        }
+      }
+      if (!listeners.length) {
+        delete this._listeners[type];
       }
     }
     return this;
-  },
+  }
+
   /**
-   * Emit event
    *
-   * <code>.emit(event[, arg1[, arg2[, ...]]])</code>
-   *
-   * @param event {string}
-   * @param *args
+   * @param {string} type
+   * @param {mixed} ...args
+   * @return {EventEmmiter}
    */
-  emit: function(event, args) {
-    if (!this._hasListeners(event)) {
-      return;
+  emit(type, ...args) {
+    // Типа браузерного onload, onclick и т.д.
+    const onfunc = 'on' + type;
+    if (this.hasOwnProperty(onfunc) && 'function' === typeof this[onfunc]) {
+      this[onfunc](...args);
     }
-    args = [].slice.call(arguments, 1);
-    var listeners = this._listeners[event];
-    var i = 0;
-    while (i < listeners.length) {
-      var listener = listeners[i];
-      listener.fn.apply(listener.scope, args)
-      if (listener.once) {
-        listeners.splice(i, 1);
-      } else {
-        ++i
+    if (this._listeners.hasOwnProperty(type)) {
+      for (const listener of this._listeners[type]) {
+        listener.apply(this, args);
       }
     }
-    if (!listeners.length) {
-      delete this._listeners[event];
-    }
-
-  },
-  _hasListeners: function(event) {
-    return this._listeners.hasOwnProperty(event);
+    return this;
   }
-};
-
-/**
- * Add mixin to obj 
- *
- * <code>
- * function Foo() {}
- *
- * EventEmitter.mixin(Foo);
- * var foo = new Foo();
- * foo.on("test", () => console.log("It's works!"));
- * foo.emit("test");
- * </code>
- *
- * @param obj {object}
- * @return {object}
- */
-EventEmitter.mixin = function(obj) {
-  if (typeof obj == "function") {
-    obj = obj.prototype;
-  }
-  var proto = new EventEmitter();
-  for (var i in proto) {
-    obj[i] = proto[i];
-  }
-  return obj;
-};
+}
