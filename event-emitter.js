25ce88d640c49a289ee8bdb17c8405b8753fa4bb
Sergey M <12753171+s3rgeym@users.noreply.github.com>
2018-03-08 22:19:22 +0300

    ES 6 Syntax

--- event-emitter.js
+++ event-emitter.js
@@ -1,86 +1,43 @@
-//
-// @author Sergei Snegirev (yamldeveloper@proton.me)
-//
+const EVENT_LISTENERS = Symbol('EVENT LISTENERS GOES HERE')
 
-// Пользовательские события
-// Частный случай реализации шаблона
-// https://en.wikipedia.org/wiki/Publish%E2%80%93subscribe_pattern
-// Интересная реализация:
-// https://fossies.org/linux/www/webkitgtk-2.15.1.tar.gz/webkitgtk-2.15.1/Source/WebInspectorUI/UserInterface/Base/Object.js#l_141
 class EventEmitter {
-  constructor() {
-    this._listeners = {};
+  constructor () {
+    this[EVENT_LISTENERS] = new Map()
   }
 
-  // !Помните, что объявление стрелочной функции вида (x) => {} аналогично
-  // function (x) {}.bind(this), т.е. стрелочные функции захватывают контекст.
-  /**
-   * Add listener.
-   * @param {string} type
-   * @param {function} listener
-   * @return {EventEmmiter}
-   */
-  on(type, listener) {
-    if ('function' !== typeof listener) {
-      throw new TypeError('listener must be a function');
+  on (event, listener) {
+    let listeners = this[EVENT_LISTENERS].get(event)
+    if (typeof listensers === 'undefined') {
+      this[EVENT_LISTENERS].set(event, (listeners = new Set())) 
     }
-    if (!this._listeners.hasOwnProperty(type)) {
-      this._listeners[type] = [];
-    }
-    this._listeners[type].push(listener);
-    return this;
+    listeners.add(listener)
+    return this
   }
 
-  /**
-    * Remove listener(s).
-    * off() --> remove all listeners
-    * off(type) --> remove all listeners of type
-    * off(type, listener) --> remove listener of type
-    * @param {string} type
-    * @param {function} listener
-    * @return {EventEmmiter}
-    */
-  off(type, listener) {
-    // удаляем всех слушателей
-    if (!arguments.length) {
-      this._listeners = {};
-    } else if (1 === arguments.length) { // удалям слушателей с типом `type`
-      if (this._listeners.hasOwnProperty(type)) {
-        delete this._listeners[type];
-      }
-    } else if (this._listeners.hasOwnProperty(type)) {
-      // удаляем слушателя `listener` с типом `type`
-      const listeners = this._listeners[type];
-      let i = listeners.length;
-      while (i--) {
-        if (listener === listeners[i]) {
-          listeners.splice(i, 1);
-        }
-      }
-      if (!listeners.length) {
-        delete this._listeners[type];
+  off (event, listener) {
+    // remove all events
+    if (arguments.length === 0) {
+      this[EVENT_LISTENERS].clear()
+    } else if (arguments.length === 1) { 
+      // remove all listeners for event
+      this[EVENT_LISTENERS].delete(event)
+    } else {
+      // remove event listener
+      const listeners = this[EVENT_LISTENERS].get(event)
+      if (listeners.delete(listener) && listeners.size === 0) {
+        this[EVENT_LISTENERS].delete(event)
       }
     }
-    return this;
+    return this
   }
-
-  /**
-   * emit( type [, arg1 [, arg2 [, ...]]] )
-   * @param {string} type
-   * @param {mixed} ...args
-   * @return {EventEmmiter}
-   */
-  emit(type, ...args) {
-    // Типа браузерного onload, onclick и т.д.
-    const onfunc = 'on' + type;
-    if (this.hasOwnProperty(onfunc) && 'function' === typeof this[onfunc]) {
-      this[onfunc](...args);
-    }
-    if (this._listeners.hasOwnProperty(type)) {
-      for (const listener of this._listeners[type]) {
-        listener.apply(this, args);
+  
+  emit (event, ...args) {
+    const listeners = this[EVENT_LISTENERS].get(event)
+    if (typeof listeners !== 'undefined') {
+      for (const listener of listeners) {
+        listener(...args) 
       }
     }
-    return this;
+    return this
   }
 }
