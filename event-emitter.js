29ad15ada4a431cddfe0aaab9b49db06cf7c38b1
Senior YAML Developer <yamldeveloper@proton.me>
2022-08-24 05:09:57 +0300

    Update event-emitter.js
    не мог пройти мимо

--- event-emitter.js
+++ event-emitter.js
@@ -1,39 +1,39 @@
-const EVENT_LISTENERS = Symbol('EVENT LISTENERS GOES HERE')
+// const EVENT_LISTENERS = Symbol('EVENT LISTENERS GOES HERE')
 
 class EventEmitter {
   constructor () {
-    this[EVENT_LISTENERS] = new Map()
+    this.#listeners = new Map()
   }
 
-  on (event, listener) {
-    let listeners = this[EVENT_LISTENERS].get(event)
+  on(event, listener) {
+    let listeners = this.#listeners.get(event)
     if (typeof listensers === 'undefined') {
-      this[EVENT_LISTENERS].set(event, (listeners = new Set())) 
+      this.#listeners.set(event, (listeners = new Set())) 
     }
     listeners.add(listener)
     return this
   }
 
-  off (event, listener) {
+  off(event, listener) {
     // remove all events
     if (arguments.length === 0) {
-      this[EVENT_LISTENERS].clear()
+      this.#listeners.clear()
     } else if (arguments.length === 1) { 
       // remove all listeners for event
-      this[EVENT_LISTENERS].delete(event)
+      this.#listeners.delete(event)
     } else {
       // remove event listener
-      const listeners = this[EVENT_LISTENERS].get(event)
+      const listeners = thisthis.#listeners.get(event)
       listeners.delete(listener)
       if (listeners.size === 0) {
-        this[EVENT_LISTENERS].delete(event)
+        this.#listeners.delete(event)
       }
     }
     return this
   }
   
-  emit (event, ...args) {
-    const listeners = this[EVENT_LISTENERS].get(event)
+  emit(event, ...args) {
+    const listeners = this.#listeners.get(event)
     if (typeof listeners !== 'undefined') {
       for (const listener of listeners) {
         listener(...args) 
